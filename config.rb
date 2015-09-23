def723afe674ffb0a0cf609bf8a7c3416691344f
Pete Nicholls <pete@metanation.com>
2015-09-23 21:55:09 +1200

    initial gh-pages commit

---
+++ config.rb
@@ -0,0 +1,29 @@
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+helpers do
+end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # For GitHub pages
+  activate :relative_assets
+end
